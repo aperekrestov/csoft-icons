d711ad0f9cd1d570b3377a432a0b1aeede6b839a
Alex <a.perekrestov@gmail.com>
2022-11-21 17:05:48 +0300

    Базовый роутинг

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,11 +1,26 @@
-import IconPage from '@containers/IconPage'
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
 
-import cn from 'classnames'
+import IconPage from '@containers/IconPage'
+import LegalPage from '@containers/LegalPage'
+
+
+
+
 import styles from './App.module.css'
 
 const App = () => {
 	return (
-		<IconPage />
+		<>
+			<BrowserRouter>
+				<NavLink to="/">Home</NavLink>
+				<NavLink to="/legal">Legal</NavLink>
+
+				<Routes>
+					<Route path="/" element={<IconPage/>} />
+					<Route path="/legal" element={<LegalPage/>} />
+				</Routes>
+			</BrowserRouter>
+		</>
 	)
 }
 
---
+++ src/containers/LegalPage/LegalPage.jsx
@@ -0,0 +1,11 @@
+import styles from './LegalPage.module.css'
+
+const LegalPage = () => {
+	return (
+		<>
+			<h1>Страница юридической информации</h1>
+		</>
+	)
+}
+
+export default LegalPage

50c179ecae7c6df97f14d2104d6bf3575992e9c8
Alex <a.perekrestov@gmail.com>
2022-12-14 17:34:49 +0300

    Добавление необходимых файлов для теста

--- src/services/context.js
+++
@@ -1,3 +0,0 @@
-import React from 'react'
-
-export const IconArray = React.createContext(null)
--- src/services/contextIconArray.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { createContext } from 'react'
-
-export const contextIconArray = React.createContext(1)
-
-
-// import React from 'react'
-
-// export const contextIconArray = React.createContext(["j,hv", "jhf"])

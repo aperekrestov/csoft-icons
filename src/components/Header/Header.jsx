4e6a798be8b1ac0050934e2833d6f4e08361adf4
Alex <a.perekrestov@gmail.com>
2023-02-07 16:03:53 +0300

    отображения списка подсказок в поиске

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types'
-import { useState, useCallback, useContext } from 'react'
+import { useState, useContext, useEffect, useRef } from 'react'
 import { NavLink, useNavigate } from 'react-router-dom'
-import { useSearchParams } from 'react-router-dom'
 
 import { IconArray } from '@context/context'
 
@@ -10,17 +9,54 @@ import styles from './Header.module.css'
 
 const Header = ({searchText=''}) => {
 	const { iconArray, setIconArray } = useContext(IconArray)
+	const [ coincidence, setCoincidence ] = useState()
 	const navigate = useNavigate()
+	const userQuery  = useRef()
+	let uniqueTags = []
+	let inputValue = ''
 
 	const handleSubmit = (e) => {
 		e.preventDefault()
-		const form = e.target;
-		const userQuery = form.search.value
-		//todo .../icons?icon=abc$id=123
-		if (userQuery !== '') {
-			navigate('/search=' + userQuery, { state: {query: userQuery} })
+		// todo .../icons?icon=abc$id=123
+		goToUserQueryPage()
+	}
+
+	const clickCoincidence = (e) => {
+		userQuery.current.value = e.target.textContent
+		goToUserQueryPage()
+	}
+
+	const handleChange = (e) => {
+		inputValue = e.target.value
+		setCoincidence(getOptions)
+		if (inputValue === '') {
+			setCoincidence('')
 		}
 	}
+
+	const goToUserQueryPage = () => {
+		if (userQuery.current.value !== '') {
+			setCoincidence('')
+			navigate('/search=' + userQuery.current.value, { state: {query: userQuery.current.value} })
+		}
+	}
+
+	function getOptions() {
+		const regex = new RegExp(inputValue, 'gi')
+		return uniqueTags.filter(item => {
+			return item.match(regex)
+		})
+	}
+
+	useEffect(()=>{
+		if(iconArray) {
+			const iconArrayTags = iconArray.map(item => item.tags.toLowerCase())
+			const iconArrayTagsJoin = iconArrayTags.join(', ')
+			const iconArrayTagsSplit = iconArrayTagsJoin.split(', ')
+			const uniqueTagsSet = new Set(iconArrayTagsSplit)
+			uniqueTags = [...uniqueTagsSet]
+		}
+	}, [iconArray, uniqueTags])
 
 
 	return (
@@ -32,19 +68,31 @@ const Header = ({searchText=''}) => {
 
 			{iconArray
 				?
-				<form autoComplete='off' onSubmit={handleSubmit} className={styles.search}>
+				<form autoComplete='off' onChange={handleChange} onSubmit={handleSubmit} className={styles.search}>
 					<input 
 						type='search' 
 						name='search' 
 						placeholder="Поиск графических иконок"
 						defaultValue={searchText}
 						className={cn(styles.search__input)} 
+						ref={userQuery}
 					/>
 					<input 
 						type='submit' 
 						value=""
 						className={styles.search__btn}
 					/>
+					
+					{coincidence &&
+						<ul className={styles.search_options}>
+							{coincidence.map((item) => 
+							<li onClick={clickCoincidence} key={item}>
+								{item}
+							</li>
+							)}
+						</ul>
+					}
+
 				</form>
 				:
 				<span className={"warning_text font_ultra text_center"}>ограниченный режим</span>

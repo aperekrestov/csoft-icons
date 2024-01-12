723a83174e1090b88eb7260077b90c02b126735e
Alex <a.perekrestov@gmail.com>
2024-01-12 12:59:02 +0300

    Обновил страницу 404

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -11,12 +11,19 @@ const NotFoundPage = () => {
 
 	return (
 		<div className="wrapper">
-			<Header searchText={""}/>
+			<Header searchText={""} />
 
 			<section className={"content_width_middle padding_top_bottom_l"}>
 				<h2>404</h2>
 				{/* todo дописать стилизацию и полный адрес ссылки */}
-				<p>По адресу: <u>{addrass}</u></p>
+				<p>К сожалению данной страницы не существует. Возможно допушена ошибка в адресной строке: <br />
+					<b>
+						<u>
+							{addrass}
+						</u>
+					</b>
+				</p>
+
 			</section>
 
 		</div>
